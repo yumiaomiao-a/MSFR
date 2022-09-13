6ad4a978f34e35bca8304f523be009f9b7552e0c
yumiaomiao-a <80734248+yumiaomiao-a@users.noreply.github.com>
2022-09-13 11:32:19 +0800

    Update train.py

--- train.py
+++ train.py
@@ -90,9 +90,9 @@ from Model import VGG_based_multi
 model = VGG_based_multi()
 
 ## load the trained model
-# dic = torch.load('xxx.pth')
+# model_dir = torch.load('xxx.pth')
 # new_state_dict = {}
-# for k,v in dic.items():
+# for k,v in model_dir.items():
 #     new_state_dict[k[7:]] = v
 # model.load_state_dict(new_state_dict)
 
